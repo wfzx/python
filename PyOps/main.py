7d084c8ee8949c9fee0be080029d73b2553e26be
Zxx <m18538209483@163.com>
2019-04-10 09:23:10 +0800

    modify

--- PyOps/main.py
+++ PyOps/main.py
@@ -6,7 +6,7 @@ LogName = "./logs/PyOps.log"
 if len(sys.argv) > 1:
     if sys.argv[1] == "ud":
         try:
-            NumOne = str(input("请输入要操作的服务器名:"))
+            NumOne = str(input("请输入要操作的服务器组名:"))
             NumTwo = str(input("请输入要操作的类型(h5,java,mb,scp,dl,dlf):"))
         except KeyboardInterrupt:
             print ("退出")
