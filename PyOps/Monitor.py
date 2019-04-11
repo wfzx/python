fe2745c6083139df55789b0f455288c0aaada1c0
Zxx <m18538209483@163.com>
2019-04-11 13:18:11 +0800

    优化Monitor获取参数的方法

--- PyOps/Monitor.py
+++ PyOps/Monitor.py
@@ -3,13 +3,17 @@
 import sys
 from PyOps import function
 
-if sys.argv[1] == "all":
-    AllServerGroupName = function.GetAllServerConnectionInformation()
-    for i in range(1,len(AllServerGroupName)):
-        function.GetServerConnectionInformation(AllServerGroupName[i])
+if len(sys.argv) > 1:
+    if sys.argv[1] == "all":
+        AllServerGroupName = function.GetAllServerConnectionInformation()
+        for i in range(1,len(AllServerGroupName)):
+            function.GetServerConnectionInformation(AllServerGroupName[i])
+            cmd = "python3 ~/python/agent.py"
+            function.ConnectToTheServer(cmd)
+    else:
+        function.GetServerConnectionInformation(sys.argv[1])
         cmd = "python3 ~/python/agent.py"
         function.ConnectToTheServer(cmd)
 else:
-    function.GetServerConnectionInformation(sys.argv[1])
-    cmd = "python3 ~/python/agent.py"
-    function.ConnectToTheServer(cmd)
+    print("Please pass the reference")
+    sys.exit()
