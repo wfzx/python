e87a66f62486246fb85af46b48b00e60bd41be20
deploy <deploy@team-lab.cn>
2019-09-27 16:28:57 +0800

    添加获取Url状态的脚本

---
+++ Curl/main.py
@@ -0,0 +1,31 @@
+#-*- coding:utf-8 -*-
+
+import sys,requests
+def GetUrlStatusCode(url):
+    try:
+        request = requests.get(url)
+        httpstatuscode = request.status_code
+        return httpstatuscode
+    except requests.exceptions.ConnectionError:
+        return 1
+    except requests.exceptions.MissingSchema:
+        return 2
+
+
+url = str(input("Please Enter Check Url :"))
+
+if url[0:7] != 'http://':
+    if url[0:8] != 'https://':
+        print ('Please in Domain Add http:// or https://')
+        sys.exit(0)
+
+Code = GetUrlStatusCode(url)
+if Code == 1:
+    print ('The domain name entered is incorrect : %s 0' % (url))
+    sys.exit(0)
+elif Code == 2:
+    print ('The domain name entered is incorrect : %s' % (url))
+    sys.exit(0)
+else:
+    print (Code)
+
